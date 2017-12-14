9d2c84a58d1fdb7bd7eebbecc03967f6003cf9d9
Thomas Abraham <methomasabraham@gmail.com>
2017-12-14 21:40:18 +0100

    Adding unit test to p07

--- list/p07.go
+++ list/p07.go
@@ -1,29 +1,11 @@
-package main
-
-import "fmt"
+package list
 
 // Problem 7
 // Flatten a nested list structure
 // Eg: [1 [2 [3 4] 5]] => [1 2 3 4 5]
 // Currently to keep things simple, we'll use a hardcoded slice
 
-func main() {
-	fmt.Println("Problem-7: Flatten a nested list")
-	inputSlice := []interface{}{
-		[]interface{}{1, 2},
-		3,
-		[]interface{}{
-			5,
-			[]interface{}{6, 7},
-		},
-		8,
-		9,
-	}
-	fmt.Println("The intput slice is", inputSlice)
-	fmt.Println("The flattened slice is", flatten(inputSlice))
-}
-
-func flatten(inputSlice []interface{}) []int {
+func Flatten(inputSlice []interface{}) []int {
 	output := []int{}
 	for _, iv := range inputSlice {
 		switch v := iv.(type) {
@@ -31,7 +13,7 @@ func flatten(inputSlice []interface{}) []int {
 			output = append(output, v)
 			break
 		case []interface{}:
-			output = append(output, flatten(v)...)
+			output = append(output, Flatten(v)...)
 		default:
 			// This will probably be an error case.
 			// Ignoring it as our input is fixed
---
+++ list/t07_test.go
@@ -0,0 +1,38 @@
+package list
+
+import (
+	"testing"
+)
+
+func Test07(t *testing.T) {
+
+	cases := []struct {
+		input  []interface{}
+		output []int
+	}{
+		{[]interface{}{
+			[]interface{}{1, 2},
+			3,
+			[]interface{}{
+				5,
+				[]interface{}{6, 7},
+			},
+			8,
+			9,
+		}, []int{1, 2, 3, 5, 6, 7, 8, 9}},
+	}
+
+	for _, c := range cases {
+		flat := Flatten(c.input)
+		if len(flat) != len(c.output) {
+			t.Errorf("Length of flattened slice is not expected value")
+		}
+
+		for i := range flat {
+			if flat[i] != c.output[i] {
+
+				t.Errorf("Result %s is not equal to expected value %s", flat, c.output)
+			}
+		}
+	}
+}
