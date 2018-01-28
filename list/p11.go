9ae5ff15030e0a2c7a701aeeecb91e963db36ec8
Thomas Abraham <methomasabraham@gmail.com>
2018-01-28 13:42:05 +0100

    Added problem 13
    I found that the solution I used for problem 11 is in fact good for problem 13. So I copied it for problems 13 and made a simpler solution for problem 11

--- list/p11.go
+++ list/p11.go
@@ -5,31 +5,18 @@ package list
 // Eg: [1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 7, 7, 7] => [[2, 1], [3, 2], 3, [2, 4], 5, 6, [3, 7]]
 
 func RunlengthEncode2(input []int) []interface{} {
+
+	encodedList := RunlengthEncode(input)
+
 	output := make([]interface{}, 0)
 
-	if len(input) == 0 {
-		return output
+	for _, v := range encodedList {
+		if v[0] > 1 {
+			output = append(output, v)
+		} else {
+			output = append(output, v[1])
+		}
 	}
 
-	currentNumber := input[0]
-	currentNumberCount := 1
-	for _, v := range input[1:] {
-		if currentNumber == v {
-			currentNumberCount++
-			continue
-		}
-		if currentNumberCount > 1 {
-			output = append(output, []int{currentNumberCount, currentNumber})
-		} else {
-			output = append(output, currentNumber)
-		}
-		currentNumber = v
-		currentNumberCount = 1
-	}
-	if currentNumberCount > 1 {
-		output = append(output, []int{currentNumberCount, currentNumber})
-	} else {
-		output = append(output, currentNumber)
-	}
 	return output
 }
